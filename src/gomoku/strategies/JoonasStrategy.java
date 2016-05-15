798b91ff5f170616e4e106a9fe7b7f7597206ee1
Joonas <joonaslume@gmail.com>
2016-05-15 17:13:55 +0300

    Assesses which tile belongs to which player

--- src/gomoku/strategies/JoonasStrategy.java
+++ src/gomoku/strategies/JoonasStrategy.java
@@ -192,6 +192,7 @@ public class JoonasStrategy implements ComputerStrategy {
     }
 
     public LocationType randomMove(){
+        board = getBoard();
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) {
                 if (board[row][col] == SimpleBoard.EMPTY) {
